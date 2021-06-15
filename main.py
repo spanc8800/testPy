25ac92d8631f5555059e5cbec02d9f4095cd42c0
spanc8800 <85808993+spanc8800@users.noreply.github.com>
2021-06-15 09:16:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("qwerqqqqqqty")
+import smtplib
+
+
+#написать сообщение на почту 
+def WriteToEMAIL(text): 
+ 
+  addr_from = "spidermanlove69@mail.ru" # Адресат 
+  addr_to = "boryasokolov@gmail.com" # Получатель 
+  password = "hero2233" # Пароль 
+
+ 
+  msg = MIMEMultipart() # Создаем сообщение 
+  msg['From'] = addr_from # Адресат 
+  msg['To'] = addr_to # Получатель 
+  msg['Subject'] = '' # Тема сообщения 
+ 
+  body = text 
+  msg.attach(MIMEText(body, 'plain')) # Добавляем в сообщение текст 
+ 
+ 
+  server = smtplib.SMTP_SSL('smtp.mail.ru', 465) # Создаем объект SMTP 
+ 
+  try: 
+  server.login(addr_from, password) # Получаем доступ 
+ 
+  server.send_message(msg) # Отправляем сообщение 
+  server.quit() # Выходим 
+ 
+  print("письмо отправленно") 
+  except: 
+  print("Ошибка ПОЧТЫ : неверный логин или пароль") 
+ 
+  
+print("lol")
+ 
+ 
